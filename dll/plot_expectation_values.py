5ab7a2d20e6d58be4abc304a65fa90d9dbf77e46
6arlos6 <cariascosm@unal.edu.co>
2024-05-28 08:20:40 -0500

    "Resultados 2"

--- dll/plot_expectation_values.py
+++ dll/plot_expectation_values.py
@@ -9,6 +9,49 @@ import numpy as np
 from .main_fun import pulse_x, pulse_z, pulse_x_with_noise
 
 
+
+# =======================================================================
+
+def plot_expect(self, out, ry_tf = False,
+                path_to_save_img = os.path.join("results_of_test","test_2"),
+                index = 0):
+    
+    label_axis = ["X", "Y", "Z"]
+    
+    fig = plt.figure(constrained_layout=True)
+    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    b = Bloch()
+    b.axes = ax1
+    b.fig = fig
+    ## normalize colors to times in tlist ##
+    nrm = mpl.colors.Normalize(0, self.delt_t*1e9)
+    colors = cm.jet(nrm(self.tlist*1e9))
+    ## add data points from expectation values ##
+    if ry_tf == True:
+        for i in range(len(out)):
+            b.add_points([out[i].expect[0], out[i].expect[1], out[i].expect[2]],'m')
+    else:
+        b.add_points([out.expect[0],out.expect[1],out.expect[2]],'m')
+    ## customize sphere properties ##
+    b.point_color=list(colors)
+    b.point_marker=['o']
+    b.point_size=[8]
+    b.view=[-9,11]
+    b.zlpos=[1.1,-1.2]
+    b.zlabel=['$\left|0\\right>$','$\left|1\\right>$']
+    ## plot sphere ##
+    b.render()
+    ## Add color bar ##
+    sm = plt.cm.ScalarMappable(cmap=cm.jet, norm=nrm)
+    sm.set_array([])  # You need to set a dummy array for the right scaling
+    cbar = plt.colorbar(sm, ax = ax1, orientation='vertical', shrink=0.5)
+    cbar.set_label('Time [ns]')
+    path_to_save_img_file = os.path.join(path_to_save_img,f"R{label_axis[index]}_withRWA_expectation_in_Bloch.pdf")
+    plt.savefig(path_to_save_img_file , format='pdf', bbox_inches = 'tight')
+    plt.show()
+
+# ==============================================================
+
 label_axis = [r"$\langle \sigma_x \rangle$",
               r"$\langle \sigma_y \rangle$",
             r"$\langle \sigma_z \rangle$"]
@@ -18,11 +61,12 @@ label_axis_file_name = ["x","y","z"]
 def plot_excharges(self, out, ry_tf = False,
                    index = 0,
                    path_to_save_img = os.path.join("results_of_test","test_2"),
-                   name_file = None):
+                   name_file = None, nick_name="Rx"):
     
     # Sino se ingresa nombre poner uno:
     if name_file is None:
         name_file = f"Expectation_val_{label_axis_file_name[index]}.pdf"
+    name_file = nick_name + "_" + name_file
     
     # Grafica del valor esperado:
     if ry_tf == False:
@@ -107,42 +151,3 @@ def plot_excharges(self, out, ry_tf = False,
     plt.savefig(path_to_save_img_file  , format='pdf', bbox_inches = 'tight')
     plt.show()
 
-# =======================================================================
-
-def plot_expect(self, out, ry_tf = False,
-                path_to_save_img = os.path.join("results_of_test","test_2"),
-                index = 0):
-    
-    label_axis = ["X", "Y", "Z"]
-    
-    fig = plt.figure(constrained_layout=True)
-    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
-    b = Bloch()
-    b.axes = ax1
-    b.fig = fig
-    ## normalize colors to times in tlist ##
-    nrm = mpl.colors.Normalize(0, self.delt_t*1e9)
-    colors = cm.jet(nrm(self.tlist*1e9))
-    ## add data points from expectation values ##
-    if ry_tf == True:
-        for i in range(len(out)):
-            b.add_points([out[i].expect[0], out[i].expect[1], out[i].expect[2]],'m')
-    else:
-        b.add_points([out.expect[0],out.expect[1],out.expect[2]],'m')
-    ## customize sphere properties ##
-    b.point_color=list(colors)
-    b.point_marker=['o']
-    b.point_size=[8]
-    b.view=[-9,11]
-    b.zlpos=[1.1,-1.2]
-    b.zlabel=['$\left|0\\right>$','$\left|1\\right>$']
-    ## plot sphere ##
-    b.render()
-    ## Add color bar ##
-    sm = plt.cm.ScalarMappable(cmap=cm.jet, norm=nrm)
-    sm.set_array([])  # You need to set a dummy array for the right scaling
-    cbar = plt.colorbar(sm, ax = ax1, orientation='vertical', shrink=0.5)
-    cbar.set_label('Time [ns]')
-    path_to_save_img_file = os.path.join(path_to_save_img,f"R{label_axis[index]}_withRWA_expectation_in_Bloch.pdf")
-    plt.savefig(path_to_save_img_file , format='pdf', bbox_inches = 'tight')
-    plt.show()
